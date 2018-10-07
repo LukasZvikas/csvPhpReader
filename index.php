52d748b1b41579fccc49d86652a8ceb3e10710e3
lukaszvikas <lzvikas1@gmail.com>
2018-10-07 19:17:34 -0400

    Feature: added class and function to read contents from csv file

--- index.php
+++ index.php
@@ -6,13 +6,32 @@
  * Time: 6:22 PM
  */
 
-Main::start();
+$csvFile = 'csvFile.csv';
 
+Main::start($csvFile);
 
 class Main {
 
-    public static function start() {
-        echo "hello there!";
+    public static function start($filename) {
+        CsvReader::readCsv($filename);
+    }
+}
+
+
+class CsvReader {
+
+    public static function readCsv($fileName) {
+
+        $f = fopen($fileName, "r");
+        $rowCount = 1;
+        while (($line = fgetcsv($f)) !== false) {
+
+            foreach ($line as $cell) {
+                echo "<td>" . $cell . "</td>";
+                $rowCount++;
+            }
+        }
+        fclose($f);
     }
 }
 
