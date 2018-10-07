9709e20dddd8fd64a36161db1addde7dbc3bda28
lukaszvikas <lzvikas1@gmail.com>
2018-10-07 18:27:16 -0400

    Feature: added Main class and start function

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukas
+ * Date: 10/7/18
+ * Time: 6:22 PM
+ */
+
+Main::start();
+
+
+class Main {
+
+    public static function start() {
+        echo "hello there!";
+    }
+}
+
+?>
