a9bffb2bc9573294d4080b1dffca2054c003171c
lukaszvikas <lzvikas1@gmail.com>
2018-10-11 09:42:43 -0400

    Task: added a record class that will be used to create record instances

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class main  {
     }
 }
 
-class records {
+class recordsGenerator {
     public static function generateRecordArray($records) {
 
         $recordArray = array();
@@ -68,6 +68,22 @@ class csvFileReader {
         }
         fclose($file);
         return $records;
+    }
+}
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name, $value) {
+        $this->{$name} = $value;
     }
 }
 
