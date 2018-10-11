0e6eca4453bb1789acbf96e6e6e2222c07c58813
lukaszvikas <lzvikas1@gmail.com>
2018-10-11 00:53:13 -0400

    Fix: deleted all of the functions, starting with reading the file and returing array of lines

--- index.php
+++ index.php
@@ -1,75 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lukas
- * Date: 10/7/18
- * Time: 6:22 PM
- */
 
-$csvFile = 'csvFile.csv';
-
-Main::start($csvFile);
-
-class Main {
-
-    public static function start($fileName) {
-        TableBuilder::buildHtmlTable($fileName);
-    }
-}
-
-
-class CsvReader {
-
-    public static function getColumnArray($line) {
-        $wordsPerLine = count($line);
-        $columnArray  = array();
-        $wordCount    = 1;
-        while ($wordCount <= $wordsPerLine) {
-            $item = "<th>" . "Column" . " " . $wordCount . "</th>";
-            array_push($columnArray, $item);
-            $wordCount++;
-        }
-        return $columnArray;
-    }
-
-    public static function readCsv($fileName) {
-
-        $f = fopen($fileName, "r");
-        $rowCount = 1;
-        while (($line = fgetcsv($f)) !== false) {
-
-            $columns = self::getColumnArray($line);
-
-            if ($rowCount == 1) {
-                echo "<tr>";
-                foreach ($columns as $column) {
-                    echo $column;
-                }
-                echo "</tr>";
-            } else
-                echo '<tr>';
-
-            foreach ($line as $cell) {
-                echo "<td>" . $cell . "</td>";
-                $rowCount++;
+class csvfileReader {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
             }
-            echo "</tr>\n";
+            $count++;
         }
-        fclose($f);
+        fclose($file);
+        return $records;
     }
 }
 
-class TableBuilder {
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
 
-    public static function buildHtmlTable($fileName)
-    {
-        echo "<html>
-                <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\">
-                <body>
-                    <table class='table table-striped'>\n\n";
-                    CsvReader::readCsv($fileName);
-        echo "\n</table></body></html>";
-    }
+        }
 }
 
 ?>
